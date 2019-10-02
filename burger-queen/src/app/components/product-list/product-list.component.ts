9eb845e092b41d2ca18f42d0a425d0acb1859017
ClaudiaQuezadaRiquelme <cklaho.quesoda.X@gmail.com>
2019-10-02 00:34:39 -0300

    se puede elegir tipo de hamburguesa

--- burger-queen/src/app/components/product-list/product-list.component.ts
+++ burger-queen/src/app/components/product-list/product-list.component.ts
@@ -4,6 +4,8 @@ import { OrdersService } from "../../services/orders.service";
 
 import {Product} from './../../models/products';
 
+// import undefined = require('firebase/empty-import');
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -17,6 +19,14 @@ export class ProductListComponent implements OnInit {
   breakfastItems:any;
 
   customerName:string = '';
+  newBurgerType:string = '';
+  burgerItem:Product ={
+    name: '',
+    menu: '',
+    price: 0,
+    type: ''
+  };
+  showKindOfBurger:boolean = false;
 
   arrayOfProducts:object[] = [];
   breakfastBool:boolean = false;
@@ -77,11 +87,48 @@ export class ProductListComponent implements OnInit {
 
   //adding and removing products of the kind Product to itemsOrder
 
+  changeKindOfBurger = (kindOfBurger:string) => {
+    let burggger:Product = {
+      menu: this.burgerItem.menu,
+      name: this.burgerItem.name,
+      price: this.burgerItem.price,
+      type: kindOfBurger
+    }
+    this.showKindOfBurger = false;
+    this.burgerItem.type = kindOfBurger;
+    this.addItem(burggger);//si pongo this.addItem(this.burgerItem), cada vez que agregue una hamburguesa con un tipo distinto, todas las hamburguesas cambiarán su tipo. Ejemplo: si elijo una beef y después una veg, ambas hamburguesas serán veg. Si elijo una beef, una veg y una chicken, las 3 hamburguesas serán chicken
+  }
+
   addItem = (item)=>{
-    this.itemsOfOrder.push(item);
-    this.totalOrderCost += item.price;
-    console.log(item.price);
-    console.log(this.totalOrderCost);
+
+    if (item.name === 'Hamburguesa Simple'||item.name === 'Hamburguesa Doble') {
+      if (item.type === 'Burger') {
+        console.log('if primero');
+        this.showKindOfBurger = true;
+        //si pongo this.burgerItem = item, sólo podemos elegir una hamburguesa Simple y una Doble por pedido y no nos permitirá elegir el tipo [veg,beef,chicken] nunca más. Esto es porque el ngFor corre sólo una vez.
+        this.burgerItem.menu = item.menu;
+        this.burgerItem.name = item.name;
+        this.burgerItem.price = item.price;
+        this.burgerItem.type = item.type;
+        console.log('this.burgerItem: ', this.burgerItem);
+  
+      } else if (this.burgerItem.type === 'chicken'||this.burgerItem.type === 'beef'||this.burgerItem.type === 'veg') {//este primero
+        console.log('if segundo');
+        
+        this.itemsOfOrder.push(item);
+        this.totalOrderCost += item.price;
+        console.log(item.price);
+        console.log(this.totalOrderCost);
+  
+      }
+    } else {
+      console.log('if por defecto');
+      this.itemsOfOrder.push(item);
+      this.totalOrderCost += item.price;
+      console.log(item.price);
+      console.log(this.totalOrderCost);
+    }
+    
   }
 
   
