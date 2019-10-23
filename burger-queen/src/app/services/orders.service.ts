e81afdad651bc0256e5b21e8e173d41d13011a3c
ClaudiaQuezadaRiquelme <cklaho.quesoda.X@gmail.com>
2019-10-23 00:49:29 -0300

    checkbox y boton para confirmar pedido. In progress

--- burger-queen/src/app/services/orders.service.ts
+++ burger-queen/src/app/services/orders.service.ts
@@ -177,4 +177,9 @@ export class OrdersService {
     return (this.initialTimeOrder - this.testingTime) / 60000;
   }
 
+  // changeStatus(id: string) {
+  //   const idInCollection = 'orders/' + id + '/status';
+  //   this.firebase.object(idInCollection).set('toDeliver');
+  // }
+
 }
